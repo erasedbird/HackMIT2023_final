096837ff055c3e71e5d4d7793b1acd6fdbcccf83
AdrianLandaverde <86375510+AdrianLandaverde@users.noreply.github.com>
2023-09-17 08:36:24 -0400

    Create translations.py

---
+++ translations.py
@@ -0,0 +1,33 @@
+english= {
+    "language":"English",
+    "name":"Reflexion Buddy",
+    "prompt1": "In a natural human way (without labelling the parts), you are providing a sentence of encouragement on the following journal entry. Then, provide a reflective question less than 15 words on the following journal entry.",
+    "prompt2":"You are providing 5 words summarizing the following journal entry.",
+    "greeting": "What is up?",
+    "think": "Let me cook...",
+    "select_language": 'Select your Language',
+    "languages":("English", "Spanish"),
+    "record": "Click to start recording",
+    "stop": "Click to stop recording",
+    "Wait":"Wait for it...",
+    "Vibe":"Your Vibe:",
+    "pdf": "Make pdf",
+    "options": "Options"
+}
+
+spanish = {
+    "language":"Spanish",
+    "name": "Compañero de Reflexión",
+    "prompt1": "De manera natural (sin etiquetar las partes), proporciona una oración de ánimo sobre la siguiente entrada en el diario. Luego, formula una pregunta reflexiva de menos de 15 palabras sobre la siguiente entrada en el diario.",
+    "prompt2": "Proporciona 5 palabras que resuman la siguiente entrada en el diario.",
+    "greeting": "¿Qué tal?",
+    "think": "Permíteme pensar...",
+    "select_language": "Selecciona tu idioma",
+    "languages": ("Inglés", "Español"),
+    "record": "Haz clic para comenzar a grabar",
+    "stop": "Haz clic para detener la grabación",
+    "Wait": "Espera un momento...",
+    "Vibe": "Tu Vibra:",
+    "pdf": "Crear PDF",
+    "options": "Opciones"
+}
